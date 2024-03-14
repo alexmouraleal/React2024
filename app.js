98e68fcd20809f7c02d5da44c820b2e9e968d3a8
alexmouraleal <alexmouraleal@gmail.com>
2024-03-14 06:36:56 -0300

    update

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import Mensagem from './components/Mensagem.js';
 
 ReactDOM.render(
   <div>
-    <Mensagem estilo="danger" texto="texto1"/>
-    <Mensagem estilo="primary" texto="texto2"/>
-    <Mensagem estilo="success" texto="texto3" />
+    <Mensagem estilo="danger" texto="erro"/>
+    <Mensagem estilo="primary" texto="normal"/>
+    <Mensagem estilo="success" texto="sucesso" />
+    <Mensagem estilo="warning" texto="validacao" />
+    <Mensagem estilo="info" texto="informacao" />
   </div>,
   document.getElementById('root')
 );
--- components/Mensagem.js
+++ components/Mensagem.js
@@ -3,18 +3,18 @@ import React, {Component} from 'react';
 
 class Mensagem extends Component {
   state = {
-    count : 0
+    count : 0 
   };
-
+  
   render() {
     let estilo = this.props.estilo;
     let texto = this.props.texto
     let alertClass = 'alert alert-'.concat(estilo);
-    
+    let aditivo = 'style="width:100px;" ' ;
     return (
       <div>
-         <div className={alertClass} role="alert">
-          <b>{texto}</b>
+         <div className={alertClass} role="alert" >
+          <b>{texto}{aditivo}</b>
          </div>
      </div>
     );
